526cbcb7a8b879f54ea7fc535cb54d722bc90836
Steffen Moeller <steffen_moeller@gmx.de>
2016-10-28 19:01:05 +0200

    Regular v15 as of 20161028

---
+++ site-ffoh.mk
@@ -0,0 +1,42 @@
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-15 \
+	gluon-alfred \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-legacy \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-mesh-vpn-fastd \
+	gluon-luci-portconfig \
+	gluon-luci-private-wifi \
+	gluon-luci-wifi-config \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-respondd \
+	gluon-setup-mode \
+	gluon-status-page \
+	iwinfo \
+	iptables \
+	haveged
+
+
+DEFAULT_GLUON_RELEASE := 0.8.5~exp$(shell date '+%Y%m%d')
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+GLUON_PRIORITY ?= 0
+
+GLUON_LANGS ?= en de
+
+GLUON_REGION=eu
+GLUON_BRANCH=experimental
+GLUON_ATH10K_MESH=11s
